da0b443f42a4708eab61d7a52d5be71547e2f430
webfans <qqqzhch@163.com>
2017-11-04 13:33:23 +0800

    duankou

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     /**
      * Node 服务器启动端口，如果是自行搭建，请保证负载均衡上的代理地址指向这个端口
      */
-    port: '5757',
+    port: '80',
     development:{
         sql: {
             dbname: 'tucao',
