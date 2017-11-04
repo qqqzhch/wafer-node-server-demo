9f6af8a15e4804998d675d4f92698be9634e481a
webfans <qqqzhch@163.com>
2017-11-04 15:15:07 +0800

    check

--- routes/gettanklist.js
+++ routes/gettanklist.js
@@ -4,12 +4,11 @@ const LoginService = require('qcloud-weapp-server-sdk').LoginService;
 let tank=require('../model/tank')
 
 module.exports = (req, res) => {
-    LoginService.create(req, res).login().then(result => {
+    LoginService.create(req, res).check().then(result => {
         tank
         .findAll({ limit: 30 ,order:[   ['createdAt', 'DESC']  ]})
         .then(function  (data) {
         	   res.json(data);
                 })
         })
-    };
-};
+    
