92602dc278841a28216018628ca1424f868a2b3f
webfans <qqqzhch@163.com>
2017-11-04 11:09:49 +0800

    日志

--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,8 @@
 const LoginService = require('qcloud-weapp-server-sdk').LoginService;
 
 module.exports = (req, res) => {
-    LoginService.create(req, res).login();
+    LoginService.create(req, res).login().then(result => {
+      console.log('wo dayin rizhi')
+        console.log('微信用户信息', result.userInfo);
+    });
 };
